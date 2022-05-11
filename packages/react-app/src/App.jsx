19326c85b01f91b7daf392146b067c0eb4d5281f
Dan Finlay <dan@danfinlay.com>
2022-05-11 10:44:26 -0700

    move files to scaffold folder

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -166,6 +166,32 @@ const web3Modal = new Web3Modal({
 });
 
 function App(props) {
+
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          MobyMask</h1>
+        <p>
+          An alliance of good-hearted phish, aiming to eliminate phishers. 
+        </p>
+      </header>
+
+      <Router>
+        <QueryParamsRoute/>
+      </Router>
+
+      <div className='footer'>
+        <p>Reporters are added on an invite-only basis.</p>
+        <p>Learn more about how our invite system works at <a href="https://github.com/danfinlay/delegatable-eth">delegatable-eth</a>.</p>
+      </div>
+
+    </div>
+  );
+
+
   const mainnetProvider =
     poktMainnetProvider && poktMainnetProvider._isProvider
       ? poktMainnetProvider
