5b2f636dd9c2d7c31d915deaa1c3bce29a012706
Dan Finlay <dan@danfinlay.com>
2022-05-12 19:12:06 -0700

    Increase gas price

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -27,7 +27,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 //
 const defaultNetwork = "localhost";
 
-const mainnetGwei = 21;
+const mainnetGwei = 85;
 
 function mnemonic() {
   try {
@@ -140,7 +140,7 @@ module.exports = {
       },
     },
     goerli: {
-      url: "https://goerli.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://goerli.infura.io/v3/" + process.env.GOERLI_INFURA_KEY, // <---- YOUR INFURA ID! (or it won't work)
 
       //      url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXXXX/eth/goerli", // <---- YOUR MORALIS ID! (not limited to infura)
 
