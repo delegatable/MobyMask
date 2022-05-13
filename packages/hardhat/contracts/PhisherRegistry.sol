35984c5457f7541d1bddf5f1e5825dc49bd5e362
Dan Finlay <dan@danfinlay.com>
2022-05-13 15:31:31 -0700

    Added tests and got working properly

--- packages/hardhat/contracts/PhisherRegistry.sol
+++ packages/hardhat/contracts/PhisherRegistry.sol
@@ -4,19 +4,21 @@ pragma solidity ^0.8.13;
 import "./Delegatable.sol";
 import "./caveat-enforcers/AbstractRevocationEnforcer.sol";
 import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "hardhat/console.sol";
 
 contract PhisherRegistry is Ownable, RevocationEnforcer {
 
   constructor(string memory name) RevocationEnforcer(name) {}
 
-  mapping (string => bool) isPhisher;
+  mapping (string => bool) public isPhisher;
   event PhisherStatusUpdated(string indexed entity, bool isPhisher);
   function claimIfPhisher (string calldata identifier, bool isAccused) onlyOwner public {
+    console.log("Claiming is Phisher %s", identifier);
     isPhisher[identifier] = isAccused;
     emit PhisherStatusUpdated(identifier, isAccused);
   }
 
-  mapping (string => bool) isMember;
+  mapping (string => bool) public isMember;
   event MemberStatusUpdated(string indexed entity, bool isMember);
   function claimIfMember (string calldata identifier, bool isNominated) onlyOwner public {
     isMember[identifier] = isNominated;
@@ -28,14 +30,17 @@ contract PhisherRegistry is Ownable, RevocationEnforcer {
    * from another class that also implements _msgSender().
    */
   function _msgSender () internal view override(Delegatable, Context) returns (address sender) {
+    console.log("msgSender() %s vs this address %s", msg.sender, address(this));
     if(msg.sender == address(this)) {
       bytes memory array = msg.data;
       uint256 index = msg.data.length;
+      console.log("Loading the crazy type of delegated msg sender");
       assembly {
         // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
         sender := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
       }
     } else {
+      console.log("Assigning sender as normal msg sender %s", msg.sender);
       sender = msg.sender;
     }
     return sender;
--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -21,16 +21,14 @@ const ownerHexPrivateKey = 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae78
 const account1PrivKey = '59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d';
 const account2PrivKey = '5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a';
 
-require('../contracts/caveat-enforcers/index.test.js');
-require('../contracts/examples/erc20.test.js');
-
 describe(CONTRACT_NAME, function () {
 
-  it('setPurpose by owner changes purpose', async () => {
-    const targetString = 'A totally new purpose!'
+  it('claimIfPhisher reports phisher', async () => {
+    const targetString = 'PhisherGuy55'
     const yourContract = await deployContract();
-    await yourContract.setPurpose(targetString);
-    expect(await yourContract.purpose()).to.equal(targetString);
+    const mined = await yourContract.claimIfPhisher(targetString, true);
+
+    expect(await yourContract.isPhisher(targetString)).to.equal(true);
   });
 
   it('other accounts cannot set purpose', async () => {
@@ -38,7 +36,7 @@ describe(CONTRACT_NAME, function () {
     const targetString = 'A totally BAD purpose!'
     const yourContract = await deployContract();
     try {
-      await yourContract.connect(addr1).setPurpose(targetString);
+      await yourContract.connect(addr1).claimIfPhisher(targetString, true);
     } catch (err) {
       expect(err.message).to.include('Ownable: caller is not the owner');
     }
@@ -52,11 +50,13 @@ describe(CONTRACT_NAME, function () {
 
     const targetString = 'A totally DELEGATED purpose!'
     const yourContract = await deployContract();
-    const util = generateUtil({
-      chainId: yourContract.chainId,
+    const { chainId } = await yourContract.provider.getNetwork();
+    const utilOpts = {
+      chainId,
       verifyingContract: yourContract.address,
       name: CONTRACT_NAME,      
-    })
+    }
+    const util = generateUtil(utilOpts);
 
     // Prepare the delegation message:
     // This message has no caveats, and authority 0,
@@ -70,7 +70,7 @@ describe(CONTRACT_NAME, function () {
     const signedDelegation = util.signDelegation(delegation, ownerHexPrivateKey);
 
     // Delegate signs the invocation message:
-    const desiredTx = await yourContract.populateTransaction.setPurpose(targetString);
+    const desiredTx = await yourContract.populateTransaction.claimIfPhisher(targetString, true);
     const delegatePrivateKey = fromHexString(account1PrivKey);
     const invocationMessage = {
       replayProtection: {
@@ -86,32 +86,30 @@ describe(CONTRACT_NAME, function () {
         },
       }],
     };
-    const typedInvocationMessage = createTypedMessage(yourContract, invocationMessage, 'Invocations', CONTRACT_NAME);
-    const invocationSig = sigUtil.signTypedData_v4(
-      delegatePrivateKey,
-      typedInvocationMessage
-    );
-    const signedInvocation = {
-      signature: invocationSig,
-      invocations: invocationMessage,
-    }
+    const signedInvocation= util.signInvocation(invocationMessage, account1PrivKey);
 
     // A third party can submit the invocation method to the chain:
     const res = await yourContract.connect(addr2).invoke([signedInvocation]);
 
     // Verify the change was made:
-    expect(await yourContract.purpose()).to.equal(targetString);
+    expect(await yourContract.connect(addr2).isPhisher(targetString)).to.equal(true);
   })
 
   it('delegates can delegate', async () => {
-    const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+    const [owner, addr1, addr2] = await ethers.getSigners();
     console.log(`owner: ${owner.address}`);
     console.log(`addr1: ${addr1.address}`);
     console.log(`addr2: ${addr2.address}`);
-    console.log(`addr3: ${addr3.address}`);
 
     const targetString = 'A totally DELEGATED purpose!'
     const yourContract = await deployContract();
+    const { chainId } = await yourContract.provider.getNetwork();
+    const utilOpts = {
+      chainId,
+      verifyingContract: yourContract.address,
+      name: CONTRACT_NAME,      
+    }
+    const util = generateUtil(utilOpts);
 
     // Prepare the delegation message:
     // This message has no caveats, and authority 0,
@@ -122,42 +120,18 @@ describe(CONTRACT_NAME, function () {
       authority: '0x0000000000000000000000000000000000000000000000000000000000000000',
       caveats: [],
     };
-    const primaryType = 'Delegation';
-    const typedMessage = createTypedMessage(yourContract, delegation, primaryType, CONTRACT_NAME);
+    const signedDelegation = util.signDelegation(delegation, ownerHexPrivateKey);
 
-    // Owner signs the delegation:
-    const privateKey = fromHexString(ownerHexPrivateKey);
-    const signature = sigUtil.signTypedData_v4(
-      privateKey,
-      typedMessage
-    );
-    const signedDelegation = {
-      signature,
-      delegation,
-    }
-
-    // First delegate signs the second delegation:
     const delegationHash = TypedDataUtils.hashStruct('SignedDelegation', signedDelegation, types, true);
     const delegation2 = {
       delegate: addr2.address,
       authority: delegationHash,
       caveats: [],
     };
-    const typedMessage2 = createTypedMessage(yourContract, delegation2, 'Delegation', CONTRACT_NAME);
+    const signedDelegation2 = util.signDelegation(delegation2, account1PrivKey);
 
-    const privateKey2 = fromHexString(account1PrivKey);
-    const signature2 = sigUtil.signTypedData_v4(
-      privateKey2,
-      typedMessage2
-    );
-    const signedDelegation2 = {
-      signature: signature2,
-      delegation: delegation2,
-    }
-
-    // Second delegate signs the invocation message:
-    const desiredTx = await yourContract.populateTransaction.setPurpose(targetString);
-    const delegatePrivateKey = fromHexString(account2PrivKey);
+    // Delegate signs the invocation message:
+    const desiredTx = await yourContract.populateTransaction.claimIfPhisher(targetString, true);
     const invocationMessage = {
       replayProtection: {
         nonce: '0x01',
@@ -172,97 +146,16 @@ describe(CONTRACT_NAME, function () {
         },
       }],
     };
-    const typedInvocationMessage = createTypedMessage(yourContract, invocationMessage, 'Invocations', CONTRACT_NAME);
-    const invocationSig = sigUtil.signTypedData_v4(
-      delegatePrivateKey,
-      typedInvocationMessage
-    );
-    const signedInvocation = {
-      signature: invocationSig,
-      invocations: invocationMessage,
-    }
-
-    // A third party can submit the invocation method to the chain:
-    const res = await yourContract.connect(addr3).invoke([signedInvocation]);
-
-    // Verify the change was made:
-    expect(await yourContract.purpose()).to.equal(targetString);
-  })
-
-  it('can allow-list a method with a caveat and it works', async () => {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    console.log(`owner: ${owner.address}`);
-    console.log(`addr1: ${addr1.address}`);
-    console.log(`addr2: ${addr2.address}`);
-
-    const targetString = 'A totally DELEGATED purpose!'
-    const yourContract = await deployContract();
-
-    const AllowListEnforcer = await ethers.getContractFactory('AllowedMethodsEnforcer');
-    const allowListEnforcer = await AllowListEnforcer.deploy();
-    await yourContract.deployed();
-    const desiredTx = await yourContract.populateTransaction.setPurpose(targetString);
-    const methodSig = desiredTx.data.substr(0, 10);
-
-    // Prepare the delegation message:
-    // This message has no caveats, and authority 0,
-    // so it is a simple delegation to addr1 with no restrictions,
-    // and will allow the delegate to perform any action the signer could perform on this contract.
-    const delegation = {
-      delegate: addr1.address,
-      authority: '0x0000000000000000000000000000000000000000000000000000000000000000',
-      caveats: [{
-        enforcer: allowListEnforcer.address,
-        terms: methodSig
-      }],
-    };
-    const typedMessage = createTypedMessage(yourContract, delegation, 'Delegation', CONTRACT_NAME);
-
-    // Owner signs the delegation:
-    const privateKey = fromHexString(ownerHexPrivateKey);
-    const signature = sigUtil.signTypedData_v4(
-      privateKey,
-      typedMessage
-    );
-    const signedDelegation = {
-      signature,
-      delegation,
-    }
-
-    // Delegate signs the invocation message:
-    const delegatePrivateKey = fromHexString(account1PrivKey);
-    const invocationMessage = {
-      replayProtection: {
-        nonce: '0x01',
-        queue: '0x00',
-      },
-      batch: [{
-        authority: [signedDelegation],
-        transaction: {
-          to: yourContract.address,
-          gasLimit: '10000000000000000',
-          data: desiredTx.data,
-        },
-      }],
-    };
-    const typedInvocationMessage = createTypedMessage(yourContract, invocationMessage, 'Invocations', CONTRACT_NAME);
-    const invocationSig = sigUtil.signTypedData_v4(
-      delegatePrivateKey,
-      typedInvocationMessage
-    );
-    const signedInvocation = {
-      signature: invocationSig,
-      invocations: invocationMessage,
-    }
+    const signedInvocation= util.signInvocation(invocationMessage, account2PrivKey);
 
     // A third party can submit the invocation method to the chain:
     const res = await yourContract.connect(addr2).invoke([signedInvocation]);
 
     // Verify the change was made:
-    expect(await yourContract.purpose()).to.equal(targetString);
-  })
+    expect(await yourContract.connect(addr2).isPhisher(targetString)).to.equal(true);
+  });
 
-  it('can allow-list a method with a caveat and disallows another method', async () => {
+  it('Revocation is available as a built-in caveat', async () => {
     const [owner, addr1, addr2] = await ethers.getSigners();
     console.log(`owner: ${owner.address}`);
     console.log(`addr1: ${addr1.address}`);
@@ -270,11 +163,13 @@ describe(CONTRACT_NAME, function () {
 
     const targetString = 'A totally DELEGATED purpose!'
     const yourContract = await deployContract();
-
-    const AllowListEnforcer = await ethers.getContractFactory('AllowedMethodsEnforcer');
-    const allowListEnforcer = await AllowListEnforcer.deploy();
-    await yourContract.deployed();
-    const desiredTx = await yourContract.populateTransaction.setPurpose(targetString);
+    const { chainId } = await yourContract.provider.getNetwork();
+    const utilOpts = {
+      chainId,
+      verifyingContract: yourContract.address,
+      name: CONTRACT_NAME,      
+    }
+    const util = generateUtil(utilOpts);
 
     // Prepare the delegation message:
     // This message has no caveats, and authority 0,
@@ -284,24 +179,17 @@ describe(CONTRACT_NAME, function () {
       delegate: addr1.address,
       authority: '0x0000000000000000000000000000000000000000000000000000000000000000',
       caveats: [{
-        enforcer: allowListEnforcer.address,
-        terms: '0x00000000', 
+        enforcer: yourContract.address,
+        terms: '0x0000000000000000000000000000000000000000000000000000000000000000',
       }],
     };
-    const typedMessage = createTypedMessage(yourContract, delegation, 'Delegation', CONTRACT_NAME);
+    const signedDelegation = util.signDelegation(delegation, ownerHexPrivateKey);
 
-    // Owner signs the delegation:
-    const privateKey = fromHexString(ownerHexPrivateKey);
-    const signature = sigUtil.signTypedData_v4(
-      privateKey,
-      typedMessage
-    );
-    const signedDelegation = {
-      signature,
-      delegation,
-    }
+    // Owner revokes outstanding delegation
+    await yourContract.revokeDelegation(signedDelegation);
 
     // Delegate signs the invocation message:
+    const desiredTx = await yourContract.populateTransaction.claimIfPhisher(targetString, true);
     const delegatePrivateKey = fromHexString(account1PrivKey);
     const invocationMessage = {
       replayProtection: {
@@ -317,22 +205,17 @@ describe(CONTRACT_NAME, function () {
         },
       }],
     };
-    const typedInvocationMessage = createTypedMessage(yourContract, invocationMessage, 'Invocations', CONTRACT_NAME);
-    const invocationSig = sigUtil.signTypedData_v4(
-      delegatePrivateKey,
-      typedInvocationMessage
-    );
-    const signedInvocation = {
-      signature: invocationSig,
-      invocations: invocationMessage,
-    }
+    const signedInvocation= util.signInvocation(invocationMessage, account1PrivKey);
 
     try {
+      // A third party can submit the invocation method to the chain:
       const res = await yourContract.connect(addr2).invoke([signedInvocation]);
     } catch (err) {
-      expect (err.message).to.include('Caveat rejected');
+      // Should not be permitted:
+      expect(err.message).to.include('Delegation has been revoked');
     }
   });
+
 });
 
 async function deployContract () {
