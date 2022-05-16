f7699a8fe44132965f534779dc5cb71018fef8cc
Dan Finlay <dan@danfinlay.com>
2022-05-16 15:23:29 -0700

    fallback for bad clipboard copying

--- packages/react-app/src/copyInvitationLink.js
+++ packages/react-app/src/copyInvitationLink.js
@@ -2,8 +2,11 @@ export default function copyInvitationLink (invitation, petName) {
   return new Promise((resolve, reject) => {
     const inviteLink = window.location.origin + '/#/members?invitation=' + encodeURIComponent(JSON.stringify(invitation));
     navigator.clipboard.writeText(inviteLink).then(function() {
-      alert('Copied to clipboard. Paste it somewhere only the intended recipients can see or you can lose your membership.');
+      alert('Your invite link has been copied to your clipboard. Paste it somewhere only the intended recipients can see or you can lose your membership: \n\n' + inviteLink);
       resolve();
+    })
+    .catch(function(err) {
+      alert('Had trouble writing to your clipboard. Here is your invite link (it may be copied). Paste it somewhere only the intended recipients can see or you can lose your membership: \n\n' + inviteLink);
     });
   });
 }
