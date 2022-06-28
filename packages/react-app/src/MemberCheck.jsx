23a3d7128b0280a3692c0c333b3144e4ee74a480
avivash <av@andrewvivash.com>
2022-06-27 22:28:20 -0700

    Feat: begin connecting MemberCheck and PhisherCheck to watcher

--- packages/react-app/src/MemberCheck.jsx
+++ packages/react-app/src/MemberCheck.jsx
@@ -1,30 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import createRegistry from './createRegistry';
-import TextInput from './TextInput';
+import { gql, useLazyQuery } from "@apollo/client";
+import createRegistry from "./createRegistry";
+import TextInput from "./TextInput";
 
-export default function MemberCheck (props) {
-  const [output, setOutput] = useState('');
+export default function MemberCheck(props) {
+  const [output, setOutput] = useState("");
   return (
     <div>
       <h3>Check Member Status</h3>
-      <TextInput placeholder="Enter a Twitter name" buttonLabel="Check"
-      onComplete={(name) => {
-        props.checkMember(name)
-        .then((result) => {
-          setOutput(result);
-        })
-        .catch(console.error);
-      }}/>
+      <TextInput
+        placeholder="Enter a Twitter name"
+        buttonLabel="Check"
+        onComplete={name => {
+          props
+            .checkMember(name)
+            .then(result => {
+              setOutput(result);
+            })
+            .catch(console.error);
+        }}
+      />
       {output ? <p>{output}</p> : null}
     </div>
-  )
+  );
 }
 
-export function MemberCheckButton (props) {
+export function MemberCheckButton(props) {
   const { provider } = props;
-  const ethersProvider = new ethers.providers.Web3Provider(provider, 'any');
-  const [ registry, setRegistry ] = useState(null);
+  const ethersProvider = new ethers.providers.Web3Provider(provider, "any");
+  const [registry, setRegistry] = useState(null);
+
+  const IS_MEMBER_GRAPHQL = `
+    query isMember($blockHash: String!, $contractAddress: String!, $key0: String!) {
+      isMember(blockHash: $blockHash, contractAddress: $contractAddress, key0: $key0) {
+        value
+        proof {
+          data
+        }
+      }
+    }
+  `;
+  const IS_MEMBER_GQL = gql(IS_MEMBER_GRAPHQL);
+  const [isMember, { loading, data }] = useLazyQuery(IS_MEMBER_GQL, {
+    context: { clientName: "watcher" },
+    variables: { blockHash: "123", contractAddress: "0x123123" }, // TODO: replace with correct values
+  });
+
+  console.log("isMember", isMember);
+  console.log("loading", loading);
+  console.log("data", data);
 
   // Get registry
   useEffect(() => {
@@ -33,26 +58,33 @@ export function MemberCheckButton (props) {
     }
 
     createRegistry(ethersProvider, true)
-    .then((_registry) => {
-      setRegistry(_registry);
-    }).catch(console.error);
+      .then(_registry => {
+        setRegistry(_registry);
+      })
+      .catch(console.error);
   });
 
   if (!registry) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
 
-  return <MemberCheck checkMember={async (name) => {
-    const codedName = `TWT:${name.toLowerCase()}`;
-    try {
-      const result = await registry.isMember(codedName);
-      if (result) {
-        return `${name} is an endorsed moby.`;
-      } else {
-        return `${name} has not been endorsed.`;
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }}/>
+  return (
+    <MemberCheck
+      checkMember={async name => {
+        const codedName = `TWT:${name.toLowerCase()}`;
+        try {
+          const result = await isMember({ variables: { key0: codedName } });
+          console.log("result", result);
+          // const result = await registry.isMember(codedName);
+          if (result) {
+            return `${name} is an endorsed moby.`;
+          } else {
+            return `${name} has not been endorsed.`;
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      }}
+    />
+  );
 }
--- packages/react-app/src/PhisherCheck.jsx
+++ packages/react-app/src/PhisherCheck.jsx
@@ -1,30 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import createRegistry from './createRegistry';
-import TextInput from './TextInput';
+import { gql, useLazyQuery } from "@apollo/client";
+import createRegistry from "./createRegistry";
+import TextInput from "./TextInput";
 
-export default function PhisherCheck (props) {
-  const [output, setOutput] = useState('');
+export default function PhisherCheck(props) {
+  const [output, setOutput] = useState("");
   return (
     <div>
       <h3>Check Phisher Status</h3>
-      <TextInput placeholder="Enter a Twitter name" buttonLabel="Check"
-      onComplete={(name) => {
-        props.checkPhisher(name)
-        .then((result) => {
-          setOutput(result);
-        })
-        .catch(console.error);
-      }}/>
+      <TextInput
+        placeholder="Enter a Twitter name"
+        buttonLabel="Check"
+        onComplete={name => {
+          props
+            .checkPhisher(name)
+            .then(result => {
+              setOutput(result);
+            })
+            .catch(console.error);
+        }}
+      />
       {output ? <p>{output}</p> : null}
     </div>
-  )
+  );
 }
 
-export function PhisherCheckButton (props) {
+export function PhisherCheckButton(props) {
   const { provider } = props;
-  const ethersProvider = new ethers.providers.Web3Provider(provider, 'any');
-  const [ registry, setRegistry ] = useState(null);
+  const ethersProvider = new ethers.providers.Web3Provider(provider, "any");
+  const [registry, setRegistry] = useState(null);
+
+  const IS_PHISHER_GRAPHQL = `
+    query isPhisher($blockHash: String!, $contractAddress: String!, $key0: String!) {
+      isPhisher(blockHash: $blockHash, contractAddress: $contractAddress, key0: $key0) {
+        value
+        proof {
+          data
+        }
+      }
+    }
+  `;
+  const IS_PHISHER_GQL = gql(IS_PHISHER_GRAPHQL);
+  const [isPhisher, { loading, data }] = useLazyQuery(IS_PHISHER_GQL, {
+    context: { clientName: "watcher" },
+    variables: { blockHash: "123", contractAddress: "0x123123" }, // TODO: replace with correct values
+  });
+
+  console.log("isPhisher", isPhisher);
+  console.log("loading", loading);
+  console.log("data", data);
 
   // Get registry
   useEffect(() => {
@@ -33,26 +58,35 @@ export function PhisherCheckButton (props) {
     }
 
     createRegistry(ethersProvider, true)
-    .then((_registry) => {
-      setRegistry(_registry);
-    }).catch(console.error);
+      .then(_registry => {
+        console.log("_registry", _registry);
+        setRegistry(_registry);
+      })
+      .catch(console.error);
   });
 
   if (!registry) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
 
-  return <PhisherCheck checkPhisher={async (name) => {
-    const codedName = `TWT:${name.toLowerCase()}`;
-    try {
-      const result = await registry.isPhisher(codedName);
-      if (result) {
-        return `${name} is an accused phisher.`;
-      } else {
-        return `${name} is not a registered phisher.`;
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }}/>
+  return (
+    <PhisherCheck
+      checkPhisher={async name => {
+        const codedName = `TWT:${name.toLowerCase()}`;
+        console.log("codedName", codedName);
+        try {
+          const result = await isPhisher({ variables: { key0: codedName } });
+          console.log("result", result);
+          // const result = await registry.isPhisher(codedName);
+          if (result) {
+            return `${name} is an accused phisher.`;
+          } else {
+            return `${name} is not a registered phisher.`;
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      }}
+    />
+  );
 }
